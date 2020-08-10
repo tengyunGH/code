f49cf1e348dbe79c88f56019bd1017dc35d79da3
tengyun <1591092672@qq.com>
2020-08-10 20:10:59 +0800

    增加一些注释

--- algorithm/src/main/java/com/life/algorithm/sort/MergeSort.java
+++ algorithm/src/main/java/com/life/algorithm/sort/MergeSort.java
@@ -11,23 +11,38 @@ public class MergeSort {
 
     public static void main(String[] args) {
         Integer[] a = {15, 2, 31, 18, 1, 90, 67, 34, 9, 23, 90, 100, 15};
+        /**
+         * 递归
+         * 传入子数组（原数组，边界）
+         * 若子数组长度等于1，返回（递归终止条件）
+         * 若子数组长度大于1，将数组分成两半，每边都进行归并排序
+         * 两半排好序之后，将整个子数组进行归并排序
+         **/
         mergeSortRecursion(a, 0, a.length - 1);
         System.out.println("结束");
 
         Integer[] b = {15, 2, 31, 18, 1, 90, 67, 34, 9, 23, 90, 100, 15};
+        /**
+         * 不递归
+         * 外层循环：子数组的长度为 1 2 4 8 16 。。。。
+         * 里层循环：子数组长度确定了，每两个子数组进行一下归并排序
+         * 注意1：当最后一个子数组的长度在i到2i之间时，注意right的指针要指向数组最后一位再进行归并排序
+         * 注意2：外层循环的i的取值范围。数组长度一般不会正好整除2
+         **/
         // 每次合并的数组的长度为 1,2,4, 8,.....
         // 注意i的取值范围
         for (int i = 1; i < b.length; i *= 2) {
             // 本循环执行的子数组个数为i，所以i的取值范围不能是i<=b.length/2，因为个数是13个的话，i大于6就会终止了
             int left = 0, center = left + i - 1, right = center + i;
-            // 对每个组进行归并排序
+            // 对每2个组进行归并排序
             while (right < b.length) {
                 merge(b, left, center, right);
                 left = right + 1;
                 center = left + i - 1;
                 right = center + i;
             }
-            // 若最后一个分组的长度比i小
+            // 出了这个循环，有三种情况，边界<left；left <= 边界 <= center；center < 边界 < right；
+            // 只有 center < 边界，这种情况，说明剩下两个组，但是最后一组的个数小于i，调整right指针
             if (center < b.length - 1) {
                 right = b.length - 1;
                 merge(b, left, center, right);
@@ -46,6 +61,16 @@ public class MergeSort {
         merge(a, left, center, right);
     }
 
+    /**
+     * 将两个有序数组合并为一个数组
+     * 将a中的后边那个数组复制出来，作为要填充的数组temp
+     * 从后往前比较，谁大谁就填充a的后边
+     * 要注意两点：1、temp填补完毕，结束循环；2、若是a的前边那个数组先填充完，那就将temp剩余的都填充到前面
+     * @param a 原数组
+     * @param left 左边界指针
+     * @param center 中心指针
+     * @param right 右指针
+     **/
     private static void merge(Integer[] a, int left, int center, int right) {
         // 将后面这个数组先复制出来
         Integer[] temp = new Integer[right - center];
@@ -55,7 +80,15 @@ public class MergeSort {
         // 此时要合并a的前半部分和temp这个数组到a里面去
         int i = temp.length - 1;
         while(left <= right) {
-            if (i < 0 || (center >= left && a[center] > temp[i])) {
+            if (i < 0) {
+                break;
+            }
+            if (center < left) {
+                a[right] = temp[i];
+                i--;
+                continue;
+            }
+            if (a[center] > temp[i]) {
                 a[right] = a[center];
                 center--;
             } else {
