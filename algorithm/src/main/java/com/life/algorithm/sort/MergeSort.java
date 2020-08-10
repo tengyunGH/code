7da915f2d25207eb57cd7ae1d8b812efabc3c6a5
tengyun <1591092672@qq.com>
2020-08-10 19:44:19 +0800

    增加归并排序的递归和不递归方法

--- algorithm/src/main/java/com/life/algorithm/sort/MergeSort.java
+++ algorithm/src/main/java/com/life/algorithm/sort/MergeSort.java
@@ -3,7 +3,7 @@ package com.life.algorithm.sort;
 /**
  * 归并排序
  * 将数组分成两个，再每个分成两个，当子数组元素个数为1的时候，进行两两的归并排序，最终形成有序的一个数组
- * 时间复杂度：
+ * 时间复杂度：nlogn
  * @author tengyun
  * @date 2020/8/10 9:06
  **/
@@ -11,17 +11,38 @@ public class MergeSort {
 
     public static void main(String[] args) {
         Integer[] a = {15, 2, 31, 18, 1, 90, 67, 34, 9, 23, 90, 100, 15};
-        mergeSort(a, 0, a.length - 1);
+        mergeSortRecursion(a, 0, a.length - 1);
+        System.out.println("结束");
+
+        Integer[] b = {15, 2, 31, 18, 1, 90, 67, 34, 9, 23, 90, 100, 15};
+        // 每次合并的数组的长度为 1,2,4, 8,.....
+        // 注意i的取值范围
+        for (int i = 1; i < b.length; i *= 2) {
+            // 本循环执行的子数组个数为i，所以i的取值范围不能是i<=b.length/2，因为个数是13个的话，i大于6就会终止了
+            int left = 0, center = left + i - 1, right = center + i;
+            // 对每个组进行归并排序
+            while (right < b.length) {
+                merge(b, left, center, right);
+                left = right + 1;
+                center = left + i - 1;
+                right = center + i;
+            }
+            // 若最后一个分组的长度比i小
+            if (center < b.length - 1) {
+                right = b.length - 1;
+                merge(b, left, center, right);
+            }
+        }
         System.out.println("结束");
     }
 
-    private static void mergeSort(Integer[] a, int left, int right) {
+    private static void mergeSortRecursion(Integer[] a, int left, int right) {
         if (left == right) {
             return;
         }
         int center = (left + right) / 2;
-        mergeSort(a, left, center);
-        mergeSort(a, center + 1, right);
+        mergeSortRecursion(a, left, center);
+        mergeSortRecursion(a, center + 1, right);
         merge(a, left, center, right);
     }
 
@@ -45,6 +66,5 @@ public class MergeSort {
         }
 
     }
-
 
 }
